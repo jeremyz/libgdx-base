18b28ebdd027826a45967d0ce1b3d1a761930118
Jérémy Zurcher <jeremy@asynk.ch>
2018-06-18 07:45:25 +0200

    Initial commit

---
+++ android/src/ch/asynk/base/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ch.asynk.base;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ch.asynk.base.MyGdxBase;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxBase(), config);
+	}
+}
---
+++ desktop/src/ch/asynk/base/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ch.asynk.base.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ch.asynk.base.MyGdxBase;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxBase(), config);
+	}
+}
